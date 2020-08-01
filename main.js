bc8337903359ad445c13de77948f31d639b72541
JavidNoshadi <javid.king63@gmail.com>
2020-08-01 23:01:41 +0430

    i changed my mind. this is going to be some images site. powerd by unsplash!

--- main.js
+++ main.js
@@ -3,28 +3,22 @@ function geeting(){
   const input = document.getElementById('input')
   const btn = document.querySelector('button')
   btn.addEventListener('click',(e)=>{
-    e.preventDefault();
-    console.log(input.value)
-    result = input.value
-    fetchingshits()
+  e.preventDefault();
+  console.log(input.value)
+  result = input.value
+  fetchingshits()
   })
-}
+  }
 geeting();
   function fetchingshits(){
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${result}&appid=7038ca96d945aa7c9f80f532ded3e38b`)
-.then(response => response.json())
-.then((data) =>{
-  res = data
-   let output = document.createElement('div')
-  output.setAttribute('class','board')
-  output.innerHTML = `
-  <h3 class='cityname'>${res.city.name}</h3>
-    <h3 class='sunrise'>${res.city.sunrise}</h3>
-  <h3 class='sunset'>${res.city.sunset}</h3>
-    <h3 class='sunset'>${res.list[0].dt}</h3> 
-  `
-  body.appendChild(output)
-  console.log(res);
-})
-.catch((error)=> console.log(error))
-  }
+    let accessKey = 'q_nWxmJ4pdcyP-fjCFz9r6Ns0Y-fEe-dAgzKWzBZkJ4'
+    let url = 'https://api.unsplash.com/search/photos?per_page=20&query=office&client_id=q_nWxmJ4pdcyP-fjCFz9r6Ns0Y-fEe-dAgzKWzBZkJ4'
+    let secretKey = 'Pm-PFZqEP3EmoYAb-rKajEbLPUkAxieZfsUpxoaG7ww'
+    fetch(url)
+    .then(response => response.json())
+    .then((data) =>{
+     res = data
+     console.log(res);
+    })
+    .catch((error)=> console.log(error))
+    }
