64ca5378f9ca3a550824e8f32199a9da73c4a877
JavidNoshadi <javid.king63@gmail.com>
2020-08-02 21:50:06 +0430

    7%!

--- main.js
+++ main.js
@@ -2,16 +2,28 @@ let body = document.querySelector('body')
 let output = document.querySelector('.output')
 let nums = 10
 let accessKey = 'q_nWxmJ4pdcyP-fjCFz9r6Ns0Y-fEe-dAgzKWzBZkJ4'
-let url = `https://api.unsplash.com/search/photos?per_page=10&query=Top&client_id=${accessKey}`
+let url = `https://api.unsplash.com/search/photos?per_page=10&query=Nature&client_id=${accessKey}`
 function defaultFetch(){
     fetch(url)
     .then(defResponse => defResponse.json())
     .then(defData => defData.results.forEach((result)=>{
       let placeholder = document.createElement('div')
-      placeholder.innerHTML = `<img src='${result.urls.small}.jpg'>`
-      console.log(placeholder)
+      placeholder.setAttribute('class','card')
+    placeholder.innerHTML = `
+    <img class='res-img' loading='lazy' src='${result.urls.thumb}.jpg'>
+    <div class='likes'>${result.likes}❤️</div>
+    <div class='dl-btn'>Download</div>
+    <div class='from'><img class='profilepic' src='${result.user.profile_image.medium}.jpg' ><a href='${result.user.links.html}'>${result.user.name}</a></div>
+    <div class='tags'>
+    <span class='tag'>#${result.tags[0].title}</span>
+    <span class='tag'>#${result.tags[1].title}</span>
+    <span class='tag'>#${result.tags[2].title}</span>
+    </div>
+    `
+      console.log(result)
       output.appendChild(placeholder)
     }) )
+    
 }
 defaultFetch()
 function geeting(){
